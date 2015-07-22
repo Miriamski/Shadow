daa4a23735f184d66e0a07f7b5c78cce38646086
Miriamski <Chamshooka@gmail.com>
2015-07-22 22:44:47 +0200

    EnterCabinetController

---
+++ Assets/EnterCabinetController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnterCabinetController : MonoBehaviour
+{
+
+    [HideInInspector] public bool isTimetraveling;
+     [HideInInspector] private bool isInReach;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    void OnTriggerEnter2D( Collider2D invader)
+    {
+        if( invader.gameObject.CompareTag("Player"))
+        {
+            isInReach = true;
+        }
+    }
+}
