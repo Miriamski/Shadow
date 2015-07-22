116eb7a8ebcb575bca26a4618a79add39f43630d
Miriamski <Chamshooka@gmail.com>
2015-07-22 19:35:59 +0200

    Cabinetdoor opens through magic

---
+++ Assets/Scripts/TimeCabinetContoller.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeCabinetContoller : MonoBehaviour
+{
+    [HideInInspector] public bool isOpen = false;
+    public Animator animator;
+
+    void OnTriggerEnter2D ( Collider2D invader)
+    {
+        if (invader.gameObject.CompareTag("Player"))
+        {
+            isOpen = true;
+        }
+    }
+
+    void OnTriggerExit2D ( Collider2D invader)
+    {
+        if (invader.gameObject.CompareTag("Player"))
+        {
+            isOpen = false;
+        }
+    }
+
+    void FixedUpdate ()
+    {
+        animate();
+    }
+
+    void animate()
+    {
+        animator.SetBool("isOpen", isOpen);
+    }
+
+
+}
