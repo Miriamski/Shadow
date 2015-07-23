1229f3e6b701df20af158f59a897e6ed28c7055d
Miriamski <Chamshooka@gmail.com>
2015-07-23 17:57:30 +0200

    Arrow only showing when inside door

--- Assets/Scripts/ArrowController.cs
+++ Assets/Scripts/ArrowController.cs
@@ -5,18 +5,29 @@ public class ArrowController : MonoBehaviour
 {
     private Animator animator;
     private GameObject player;
+    private SpriteRenderer renderer;
     private bool inReach;
 
     void Start()
     {
         player = GetComponentInParent<EnterCabinetColliderController>().player;
         animator = GetComponent<Animator>();
+        renderer = GetComponent<SpriteRenderer>();
+        renderer.enabled = false;
         inReach = false;
     }
 
     void Update()
     {
         inReach = player.GetComponent<PlayerEntersCabinetBehaviour>().GetInReach();
+        if(inReach)
+        {
+            renderer.enabled = true;
+        }
+        else
+        {
+            renderer.enabled = false;
+        }
     }
     
     void FixedUpdate()
